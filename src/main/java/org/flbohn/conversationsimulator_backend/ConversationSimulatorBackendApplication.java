caa19a2a429708839993238eb728bd768182e238
Florian <florianbohn@arcor.de>
2024-04-27 12:42:54 +0200

    Initial Spring commit.

---
+++ src/main/java/org/flbohn/conversationsimulator_backend/ConversationSimulatorBackendApplication.java
@@ -0,0 +1,13 @@
+package org.flbohn.conversationsimulator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversationSimulatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversationSimulatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/flbohn/conversationsimulator_backend/ConversationSimulatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flbohn.conversationsimulator_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversationSimulatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
